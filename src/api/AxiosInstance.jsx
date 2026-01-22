926990cabdcf79f1a578ae5b36ef20d244b2c1ec
Sarthak Padamwar <padamwarsarthak2004@gmail.com>
2026-01-22 14:17:15 +0530

    updated code

--- src/api/AxiosInstance.jsx
+++ src/api/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.BASE_URL,
 });
 
 axiosInstance.interceptors.request.use((config) => {
